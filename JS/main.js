5c39ca6f3f54ac5b9dbfbac5ea8dc0eb116d65a0
Andrew Long <andrew@andrews-mbp.home>
2020-12-14 13:07:38 +0000

    moved nav js into external file

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,8 @@
+///////////////////////////
+// NAVIGATION
 
-//document.getElementById("#").style.color = "blue";
+$(document).ready(function() { // this closes the navigation menu wherever we click on the screen
+    $(".nav-close").click(function() { // when click an element with .nav-close on it, run the following function...
+        $("#nav-toggle").prop("checked", false); // take the element with ID="nav-toggle" (this is the checkbox) and change it's checked property to false (i.e. uncheck the box). Unchecking the box will have the effect of closing the nav
+    });
+});
